0011f2d19e8457c79e8443ea74d6dbd0df54219c
ArkanZAG <142731011+ArkanZAG@users.noreply.github.com>
2024-10-07 15:29:39 +0700

    Scene Bus

--- Assets/Scripts/Controller/TaxiController.cs
+++ Assets/Scripts/Controller/TaxiController.cs
@@ -11,6 +11,7 @@ namespace Controller
         [SerializeField] private int hourDuration;
         [SerializeField] private RandomEvent randomEvent;
         [SerializeField] private SceneController sceneController;
+        [SerializeField] private GameObject arriveUI;
         
         private void Awake()
         {
@@ -18,12 +19,14 @@ namespace Controller
             startHour = GameTime.Hours;
             randomEvent.DoEvent();
             GameTime.PauseState(false);
+            arriveUI.SetActive(false);
         }
         
         private void Update()
         {
             if ((startHour + hourDuration) > GameTime.Hours) return;
             GameTime.PauseState(true);
+            arriveUI.SetActive(true);
             if (!Input.GetKeyDown(KeyCode.Space)) return;
             sceneController.ChangeScene(WorkingState.GetWorkingState() == false ? "SceneKantor" : "MainScene");
         }
