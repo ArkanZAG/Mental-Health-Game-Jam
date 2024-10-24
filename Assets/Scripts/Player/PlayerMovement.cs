4b1aca44a900b68160c70353a1778cf4d578d2f7
ArkanZAG <142731011+ArkanZAG@users.noreply.github.com>
2024-10-24 15:57:33 +0700

    Player Animatiion

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int playerMovementSpeed;
     [SerializeField] private Rigidbody playerRigidBody;
+    
+    [SerializeField] private Animator anim;
 
     private void Movement()
     {
@@ -14,6 +16,25 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRigidBody.velocity = new Vector3(horizontalInput * playerMovementSpeed, verticalInput * playerMovementSpeed, 0);
+
+        if (horizontalInput >= 0.1f)
+        {
+            transform.localScale = Vector3.one;
+        }else if (horizontalInput <= -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        if (verticalInput != 0 || horizontalInput != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        
+        
     }
 
     private void Update()
